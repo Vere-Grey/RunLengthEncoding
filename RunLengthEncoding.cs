15b4b1dfcd9fbc18c4bd482bb16b8ba083e0d8f3
Cihlář Martin <Martin.Cihlar@ysoft.com>
2022-03-27 09:07:10 +0200

    resharper

--- RunLengthEncoding.cs
+++ RunLengthEncoding.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Linq;
 using System.Text;
 
@@ -8,31 +7,32 @@ public static class RunLengthEncoding
 
     public static string Encode(string input)
     {
-       char? currentGroup = null;
-       var counter = 0;
-       var result = new StringBuilder();
+        char? currentGroup = null;
+        var counter = 0;
+        var result = new StringBuilder();
 
-       foreach (var letter in input)
-       {
-           if (currentGroup != letter & counter > 0)
-           {
-               result.Append(EncodeGroup(counter, currentGroup));
-               currentGroup = letter;
-               counter = 1;
-           }
-           else
-           {
-               currentGroup = letter;
-               counter++;
-           }
-       }
-       result.Append(EncodeGroup(counter, currentGroup));
-       return result.ToString();
+        foreach (var letter in input)
+        {
+            if ((currentGroup != letter) & (counter > 0))
+            {
+                result.Append(EncodeGroup(counter, currentGroup));
+                currentGroup = letter;
+                counter = 1;
+            }
+            else
+            {
+                currentGroup = letter;
+                counter++;
+            }
+        }
+
+        result.Append(EncodeGroup(counter, currentGroup));
+        return result.ToString();
     }
 
     private static int parseNextMultiplier(string input, ref int index)
     {
-        var number =  new String (input.TakeWhile(Char.IsDigit).ToArray());
+        var number = new string(input.TakeWhile(char.IsDigit).ToArray());
         index += number.Length;
         return number == "" ? 1 : int.Parse(number);
     }
@@ -41,7 +41,7 @@ public static class RunLengthEncoding
     {
         var result = new StringBuilder();
 
-        for(var index = 0 ; index < input.Length; index++)
+        for (var index = 0; index < input.Length; index++)
         {
             var multiplier = parseNextMultiplier(input.Substring(index), ref index);
             var letter = input.Substring(index).First();
