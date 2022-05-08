0deecb892a5b794ad7e16ef11b125833a2dbdb2b
cihlar <martin.cihlar@ysoft.com>
2022-05-08 10:31:50 +0200

    Decode stream working

--- RunLengthEncoding.cs
+++ RunLengthEncoding.cs
@@ -1,39 +1,12 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 public static class RunLengthEncoding
 {
-    private static string EncodeGroup(int counter, char? letter) => counter > 1 ? $"{counter}{letter}" : $"{letter}";
-
-    private static IEnumerable<char> EncodeStreaming(IEnumerable<char> input)
+    private static IEnumerable<char> YieldGroup(int counter, char? currentGroup)
     {
-        char? currentGroup = null;
-        var counter = 0;
-        foreach (var letter in input)
-        {
-            var hasGroupEnded = currentGroup != letter;
-            if (hasGroupEnded & (counter > 0))
-            {
-                if (counter > 1)
-                {
-                    foreach (var digit in counter.ToString())
-                    {
-                        yield return digit;
-                    }
-                }
-
-                yield return (char)currentGroup;
-                currentGroup = letter;
-                counter = 1;
-            }
-            else
-            {
-                currentGroup = letter;
-                counter++;
-            }
-        }
-
         if (counter > 1)
         {
             foreach (var digit in counter.ToString())
@@ -45,53 +18,60 @@ public static class RunLengthEncoding
         yield return (char)currentGroup;
     }
 
+    private static IEnumerable<char> EncodeStreaming(IEnumerable<char> input)
+    {
+        char? currentGroup = null;
+        var counter = 0;
+        foreach (var letter in input)
+        {
+            var hasGroupEnded = currentGroup != letter;
+            if (hasGroupEnded & (counter > 0))
+            {
+                foreach (var character in YieldGroup(counter, currentGroup))
+                {
+                    yield return character;
+                }
+                currentGroup = letter;
+                counter = 1;
+            }
+            else
+            {
+                currentGroup = letter;
+                counter++;
+            }
+        }
+
+        foreach (var character in YieldGroup(counter, currentGroup))
+        {
+            yield return character;
+        }
+    }
+
     public static string Encode(string input) =>
         input == "" ? "" : new string(EncodeStreaming(input.ToCharArray()).ToArray());
 
-    // public static string EncodeMemoryHungry(string input)
-    // {
-    //     char? currentGroup = null;
-    //     var counter = 0;
-    //     var result = new StringBuilder();
-    //
-    //     foreach (var letter in input)
-    //     {
-    //         var doesLetterDiffer = currentGroup != letter;
-    //         if (doesLetterDiffer & (counter > 0))
-    //         {
-    //             result.Append(EncodeGroup(counter, currentGroup));
-    //             currentGroup = letter;
-    //             counter = 1;
-    //         }
-    //         else
-    //         {
-    //             currentGroup = letter;
-    //             counter++;
-    //         }
-    //     }
-    //
-    //     result.Append(EncodeGroup(counter, currentGroup));
-    //     return result.ToString();
-    // }
+    public static string Decode(string input) =>
+        input == "" ? "" : new string(DecodeStreaming(input.ToCharArray()).ToArray());
 
-    private static int TakeNextCounter(string input, ref int index)
+    private static IEnumerable<char> DecodeStreaming(IEnumerable<char> input)
     {
-        var digits = new string(input.TakeWhile(char.IsDigit).ToArray());
-        index += digits.Length;
-        return digits == "" ? 1 : int.Parse(digits);
-    }
-
-    public static string Decode(string input)
-    {
-        var result = new StringBuilder();
-
-        for (var index = 0; index < input.Length; index++)
+        string? counterBuilder = null;
+        var inputEnumenator = input.GetEnumerator();
+        while (inputEnumenator.MoveNext())
         {
-            var counter = TakeNextCounter(input.Substring(index), ref index);
-            var letter = input.Substring(index).First();
-            result.Append(letter, counter);
-        }
+            if(char.IsDigit(inputEnumenator.Current))
+            {
+                counterBuilder += inputEnumenator.Current;
+                continue;
+            }
 
-        return result.ToString();
+            var counter = int.Parse(counterBuilder ?? "1");
+            for (var i = 0; i < counter; i++)
+            {
+                yield return inputEnumenator.Current;
+            }
+
+            counterBuilder = null;
+        }
     }
 }
